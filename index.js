1ba09fdb41d7c28f89541ba7e01e924b19ab27f8
Matthew Kirby <matt.kirby@yahoo.com>
2020-10-26 15:54:51 -0400

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(arrayOfNames, event) {
+    let cards = []
+    for (let i = 0; i < arrayOfNames.length; i++) {
+        let name = arrayOfNames[i]
+        cards.push(`Thank you, ${name}, for the wonderful ${event} gift!`)
+    }
+    return cards
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num)
+        num--
+    } 
+}
